277d7ef926ec1c3e4edffba45e5f70da871448e6
Jdway <jdw.davidjaw@gmail.com>
2018-06-20 13:07:37 +0800

    modifications to test, training
    * updates BN parameters
    * a better way to determine the network type of testing

--- inference_through_logs.sh
+++ inference_through_logs.sh
@@ -1,5 +1,5 @@
 for name in "$@"
 do
-  python3 test_validation.py --model_name "teacher.ckpt-$name"
+  python3 test_validation.py --model_name "student.ckpt-$name"
 done
 
--- test_validation.py
+++ test_validation.py
@@ -8,14 +8,18 @@ import numpy as np
 def get_args():
     parser = argparse.ArgumentParser(description='Face recognition')
     parser.add_argument('--data_path', type=str, default='./dlcv_final_2_dataset/', help='path to dataset folder')
-    parser.add_argument('--weight_path', type=str, default='./log/', help='path to store/read weights')
-    parser.add_argument('--model_name', type=str, default='teacher.ckpt-224000', help='filename of trained model')
+    parser.add_argument('--weight_path', type=str, default='./weight/', help='path to store/read weights')
+    parser.add_argument('--model_name', type=str, default='{}.ckpt', help='filename of trained model')
     parser.add_argument('--batch_size', type=int, default=100, help='size of training batch')
+    parser.add_argument('--is_teacher', action='store_true', help='Either use the teacher network or student network')
     return parser.parse_args()
 
 
 def main(args):
+    if '{}' in args.model_name:
+        args.model_name = args.model_name.format('teacher' if args.is_teacher else 'student')
     print(args)
+
     with tf.variable_scope('Data_Generator'):
         data_reader = DataReader(
             data_path=args.data_path
@@ -23,9 +27,13 @@ def main(args):
         valid_x, valid_y = data_reader.get_instance(batch_size=args.batch_size, mode='valid')
         valid_num = len(data_reader.valid_img_path)
 
-    network = model.TeacherNetwork()
-    v_logits, v_net_dict = network.build_network(valid_x, class_num=len(data_reader.dict_class.keys()), reuse=False,
-                                                 is_train=False, dropout=1)
+    if args.is_teacher:
+        network = model.TeacherNetwork()
+        v_logits, v_net_dict = network.build_network(valid_x, class_num=len(data_reader.dict_class.keys()), reuse=False,
+                                                     is_train=False, dropout=1)
+    else:
+        network = model.StudentNetwork(len(data_reader.dict_class.keys()))
+        v_logits, v_pre_logit = network.build_network(valid_x, False, False)
     v_pred = tf.nn.softmax(v_logits, -1)
     v_pred = tf.argmax(v_pred, -1, output_type=tf.int32)
 
