aadcb8023b12baa95128777addd3193aa4209fdd
Jdway <jdw.davidjaw@gmail.com>
2018-06-28 12:34:05 +0800

    network selection options for test.py

--- test.py
+++ test.py
@@ -3,16 +3,19 @@ import model
 from data_reader import TestDataReader, DataReader
 import argparse
 import os
+import time
 
 
 def get_args():
     parser = argparse.ArgumentParser(description='Face recognition')
     parser.add_argument('--data_path', type=str, default='./dlcv_final_2_dataset/', help='path to dataset folder')
-    parser.add_argument('--test_data_path', type=str, default='./dlcv_final_2_dataset/test/', help='path to test folder')
+    parser.add_argument('--test_data_path', type=str, default='./dlcv_final_2_dataset/test/',
+                        help='path to test folder')
     parser.add_argument('--weight_path', type=str, default='./weight/', help='path to store/read weights')
     parser.add_argument('--model_name', type=str, default='teacher.ckpt', help='filename of trained model')
     parser.add_argument('--batch_size', type=int, default=100, help='size of training batch')
     parser.add_argument('--out_path', type=str, default='./out/', help='path to output file')
+    parser.add_argument('--is_teacher', action='store_true', help='Either use the teacher network or student network')
     return parser.parse_args()
 
 
@@ -23,8 +26,13 @@ def main(args):
         data_reader = DataReader(data_path=args.data_path)
         test_x, test_num = test_data_reader.get_instance(batch_size=args.batch_size)
 
-    network = model.TeacherNetwork()
-    logits, net_dict = network.build_network(test_x, class_num=len(data_reader.dict_class.keys()), reuse=False, is_train=False, dropout=1)
+    if args.is_teacher:
+        network = model.TeacherNetwork()
+        logits, _ = network.build_network(test_x, class_num=len(data_reader.dict_class.keys()), reuse=False,
+                                          is_train=False, dropout=1)
+    else:
+        network = model.StudentNetwork(len(data_reader.dict_class.keys()))
+        logits, _ = network.build_network(test_x, False, False)
     pred = tf.nn.softmax(logits, -1)
     pred = tf.argmax(pred, -1, output_type=tf.int32)
 
@@ -46,10 +54,12 @@ def main(args):
     out_f.write('id,ans\n')
     instance_cnt = 0
     step = 0
+    total_time = 0
     while step * args.batch_size < test_num:
         step += 1
-
+        start_time = time.time()
         np_pred = sess.run(pred)
+        total_time += time.time() - start_time
         for i in range(np_pred.shape[0]):
             out_f.write('{},{}\n'.format(instance_cnt + 1, data_reader.dict_class[np_pred[i]]))
             instance_cnt += 1
@@ -59,8 +69,9 @@ def main(args):
     out_f.close()
     coord.request_stop()
     coord.join(threads)
+    print('Total time: {:.2f} secs with {:d} instance. fps={:.2f}'.format(
+        total_time, step * args.batch_size, step * args.batch_size / total_time))
 
 
 if __name__ == '__main__':
     main(get_args())
-
